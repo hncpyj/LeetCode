575660c1652be3a0a91fdffcd9bce65186f83af9
Hailey <62372263+hncpyj@users.noreply.github.com>
2023-09-11 17:25:47 +0100

    Time: 70 ms (59.54%), Space: 17.5 MB (50.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,9 @@
-import numpy as np
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result = []
+        hashmap = {}
         for i in range(len(nums)):
-            for j in range(len(nums)):
-                if (i!=j):
-                    sum = nums[i] + nums[j]
-                    if(sum == target):
-                        result.append(i)
-                        result.append(j)
-                        return result
+            value = target - nums[i]
+            if value in hashmap:
+                return [i, hashmap[value]]
+            hashmap[nums[i]] = i
         
