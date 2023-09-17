34941e32c74241a2fa44838473b783e076487102
Hailey <62372263+hncpyj@users.noreply.github.com>
2023-09-17 01:54:08 +0100

    Time: 27 ms (98.78%), Space: 16.4 MB (35.44%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -4,16 +4,13 @@ class Solution:
         stack_t = []
         
         for i in range(max(len(s), len(t))):
-            print("[", i,"] len_s: ", len(s), " & len_t: ", len(t))
             if (i<len(s)):
-                print("stack_s: ", stack_s)
                 if (s[i] == '#'):
                     if stack_s != []:
                         stack_s.pop()
                 else:
                     stack_s.append(s[i])
             if (i<len(t)):
-                print("stack_t: ", stack_t)
                 if (t[i] == '#'):
                     if stack_t != []:
                         stack_t.pop()
