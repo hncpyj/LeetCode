7c5b25a0b0626913443e0fbe296c41d1b742a21a
Hailey <62372263+hncpyj@users.noreply.github.com>
2023-09-22 07:54:20 +0100

    Time: 45 ms (82.32%), Space: 17.8 MB (32.60%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,7 +3,6 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         if s is None or len(s) == 1:
             return True
-        lower_s = s.lower()
-        alphanum = re.sub(r'[^a-z0-9]','', lower_s)
+        alphanum = re.sub(r'[^a-z0-9]','', s.lower())
 
         return True if alphanum==alphanum[::-1] else False
