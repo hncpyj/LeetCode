dafa4a32e129f7d34ccb41f20218571c6f321aa0
Hailey <62372263+hncpyj@users.noreply.github.com>
2023-09-22 07:53:53 +0100

    Time: 40 ms (94.24%), Space: 17.9 MB (27.31%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,10 +4,6 @@ class Solution:
         if s is None or len(s) == 1:
             return True
         lower_s = s.lower()
-        alpanum = re.sub(r'[^a-z0-9]','', lower_s)
-        
-        for i in range(int(len(alpanum)/2)):
-            if alpanum[i] != alpanum[-i-1]:
-                return False
+        alphanum = re.sub(r'[^a-z0-9]','', lower_s)
 
-        return True
+        return True if alphanum==alphanum[::-1] else False
