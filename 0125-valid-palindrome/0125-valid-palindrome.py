680abffecb909bb9f8d414ef0d6adc056530a743
Hailey <62372263+hncpyj@users.noreply.github.com>
2023-09-22 07:56:12 +0100

    Time: 36 ms (98.27%), Space: 18 MB (20.95%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,6 +1,8 @@
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        if s is None or len(s) == 1:
+            return True
         lower_s = s.lower()
         alphanum = re.sub(r'[^a-z0-9]','', s.lower())
 
